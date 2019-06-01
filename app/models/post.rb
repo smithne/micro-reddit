1a4f95f14924236642b9abc49067775ab5484a94
Nick Smith <smithne@uw.edu>
2019-06-01 10:43:40 -0700

    Add post model

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 255 }
+  validates :user_id, presence: true
+end
